b0d5d36e7ac7dae18dbaac98dbd610989b111f42
Mercymaryantony <mercyantony1112@gmail.com>
2024-06-07 13:34:24 +0530

    routing yet to complete

---
+++ src/components/ViewAll.jsx
@@ -0,0 +1,180 @@
+import React from 'react'
+import NavBar from './NavBar'
+
+const ViewAll = () => {
+    return (
+        <div>
+            <NavBar/>
+            <div className="container">
+                <div className="row">
+                    <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                        <div className="row g-3">
+                            <div className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
+                                <div className="card" >
+                                    <img src="https://www.japantimes.co.jp/japantimes/uploads/images/2023/09/03/248489.jpg" className="card-img-top" alt="..." height="250px">
+                                        </img>
+                                        <div className="card-body">
+                                            <h5 className="card-title">AAAA</h5>
+                                            <p className="card-text">CLASS -12</p>
+                                            <a href="#" className="btn btn-primary">CLICK HERE</a>
+                                        </div>
+
+                                </div>
+                            </div>
+
+
+                            <div className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
+                                <div className="card" >
+                                    <img src="https://www.japantimes.co.jp/japantimes/uploads/images/2023/09/03/248489.jpg" className="card-img-top" alt="..." height="250px">
+                                        </img>
+                                        <div className="card-body">
+                                            <h5 className="card-title">AAAA</h5>
+                                            <p className="card-text">CLASS -12</p>
+                                            <a href="#" className="btn btn-primary">CLICK HERE</a>
+                                        </div>
+
+                                </div>
+                            </div>
+                            <div className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
+                                <div className="card" >
+                                    <img src="https://www.japantimes.co.jp/japantimes/uploads/images/2023/09/03/248489.jpg" className="card-img-top" alt="..." height="250px">
+                                        </img>
+                                        <div className="card-body">
+                                            <h5 className="card-title">AAAA</h5>
+                                            <p className="card-text">CLASS -12</p>
+                                            <a href="#" className="btn btn-primary">CLICK HERE</a>
+                                        </div>
+
+                                </div>
+                            </div>
+                            <div className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
+                                <div className="card" >
+                                    <img src="https://www.japantimes.co.jp/japantimes/uploads/images/2023/09/03/248489.jpg" className="card-img-top" alt="..." height="250px">
+                                        </img>
+                                        <div className="card-body">
+                                            <h5 className="card-title">AAAA</h5>
+                                            <p className="card-text">CLASS -12</p>
+                                            <a href="#" className="btn btn-primary">CLICK HERE</a>
+                                        </div>
+
+                                </div>
+                            </div>
+                            <div className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
+                                <div className="card" >
+                                    <img src="https://www.japantimes.co.jp/japantimes/uploads/images/2023/09/03/248489.jpg" className="card-img-top" alt="..." height="250px">
+                                        </img>
+                                        <div className="card-body">
+                                            <h5 className="card-title">AAAA</h5>
+                                            <p className="card-text">CLASS -12</p>
+                                            <a href="#" className="btn btn-primary">CLICK HERE</a>
+                                        </div>
+
+                                </div>
+                            </div>
+                            <div className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
+                                <div className="card" >
+                                    <img src="https://www.japantimes.co.jp/japantimes/uploads/images/2023/09/03/248489.jpg" className="card-img-top" alt="..." height="250px">
+                                        </img>
+                                        <div className="card-body">
+                                            <h5 className="card-title">AAAA</h5>
+                                            <p className="card-text">CLASS -12</p>
+                                            <a href="#" className="btn btn-primary">CLICK HERE</a>
+                                        </div>
+
+                                </div>
+                            </div>
+                            <div className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
+                                <div className="card" >
+                                    <img src="https://www.japantimes.co.jp/japantimes/uploads/images/2023/09/03/248489.jpg" className="card-img-top" alt="..." height="250px">
+                                        </img>
+                                        <div className="card-body">
+                                            <h5 className="card-title">AAAA</h5>
+                                            <p className="card-text">CLASS -12</p>
+                                            <a href="#" className="btn btn-primary">CLICK HERE</a>
+                                        </div>
+
+                                </div>
+                            </div>
+                            <div className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
+                                <div className="card" >
+                                    <img src="https://www.japantimes.co.jp/japantimes/uploads/images/2023/09/03/248489.jpg" className="card-img-top" alt="..." height="250px">
+                                        </img>
+                                        <div className="card-body">
+                                            <h5 className="card-title">AAAA</h5>
+                                            <p className="card-text">CLASS -12</p>
+                                            <a href="#" className="btn btn-primary">CLICK HERE</a>
+                                        </div>
+
+                                </div>
+                            </div>
+                            <div className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
+                                <div className="card" >
+                                    <img src="https://www.japantimes.co.jp/japantimes/uploads/images/2023/09/03/248489.jpg" className="card-img-top" alt="..." height="250px">
+                                        </img>
+                                        <div className="card-body">
+                                            <h5 className="card-title">AAAA</h5>
+                                            <p className="card-text">CLASS -12</p>
+                                            <a href="#" className="btn btn-primary">CLICK HERE</a>
+                                        </div>
+
+                                </div>
+                            </div>
+                            <div className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
+                                <div className="card" >
+                                    <img src="https://www.japantimes.co.jp/japantimes/uploads/images/2023/09/03/248489.jpg" className="card-img-top" alt="..." height="250px">
+                                        </img>
+                                        <div className="card-body">
+                                            <h5 className="card-title">AAAA</h5>
+                                            <p className="card-text">CLASS -12</p>
+                                            <a href="#" className="btn btn-primary">CLICK HERE</a>
+                                        </div>
+
+                                </div>
+                            </div>
+                            <div className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
+                                <div className="card" >
+                                    <img src="https://www.japantimes.co.jp/japantimes/uploads/images/2023/09/03/248489.jpg" className="card-img-top" alt="..." height="250px">
+                                        </img>
+                                        <div className="card-body">
+                                            <h5 className="card-title">AAAA</h5>
+                                            <p className="card-text">CLASS -12</p>
+                                            <a href="#" className="btn btn-primary">CLICK HERE</a>
+                                        </div>
+
+                                </div>
+                            </div>
+                            <div className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
+                                <div className="card" >
+                                    <img src="https://www.japantimes.co.jp/japantimes/uploads/images/2023/09/03/248489.jpg" className="card-img-top" alt="..." height="250px">
+                                        </img>
+                                        <div className="card-body">
+                                            <h5 className="card-title">AAAA</h5>
+                                            <p className="card-text">CLASS -12</p>
+                                            <a href="#" className="btn btn-primary">CLICK HERE</a>
+                                        </div>
+
+                                </div>
+                            </div>
+                            <div className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
+                                <div className="card" >
+                                    <img src="https://www.japantimes.co.jp/japantimes/uploads/images/2023/09/03/248489.jpg" className="card-img-top" alt="..." height="250px">
+                                        </img>
+                                        <div className="card-body">
+                                            <h5 className="card-title">AAAA</h5>
+                                            <p className="card-text">CLASS -12</p>
+                                            <a href="#" className="btn btn-primary">CLICK HERE</a>
+                                        </div>
+
+                                </div>
+                            </div>
+
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default ViewAll
